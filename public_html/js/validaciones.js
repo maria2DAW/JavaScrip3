bad4d56447380df83b07fbb8e6dbaece0deb7551
Maria <Maria@192.168.1.49>
2018-03-23 11:55:10 +0100

    Dar función a los botones "Enviar" y "Limpiar". Formas de acceder a un elemento en JavaScript.

--- public_html/js/validaciones.js
+++ public_html/js/validaciones.js
@@ -1,7 +1,25 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+function validarForm()
+{
+    alert("Validando");
+    document.contacto_frm.submit();
+}
 
+function limpiarForm()
+{
+    alert("Limpiando");
+    document.getElementById("contacto_frm").reset();
+}
 
+window.onload = function()
+{
+    var botonValidar, botonLimpiar;
+    
+    //Formas de acceder a un elemento en JavaScript
+    //1-Por el método "getElementById"
+    botonLimpiar = document.getElementById("limpiar");
+    botonLimpiar.onclick = limpiarForm;
+    
+    //2-Por ruta a través del name de las etiquetas HTML
+    botonValidar = document.contacto_frm.enviar_btn;    
+    botonValidar.onclick = validarForm;
+}
